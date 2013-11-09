060e79aca7fd1cf56d2e3ff6711cdb2f73c26f68
Filirom1 <Filirom1@gmail.com>
2013-11-09 08:05:26 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,240 @@
+var fs = require('fs');
+var rimraf = require('rimraf');
+var yaml = require('js-yaml');
+var _ = require('underscore');
+var async = require('async');
+var stomp = require('stomp');
+
+exports = module.exports = OORouter;
+
+function OORouter(options){
+  this.options = options;
+  this.routes = {};
+}
+
+// send `kill -HUP pid` maximum every seconds, even if this function is called more.
+OORouter.prototype.reloadNginx = _.throttle(function(){
+  process.kill('HUP', this.options.nginxPid);
+}, 1000, {leading: false, trailing: true})
+
+// Listen on activeMQ, and parse message body from yaml
+OORouter.prototype.listenOnActiveMq = function(){
+  this.client = new stomp.Stomp({
+    port: this.options.activemq_port,
+    host: this.options.activemq_host,
+    login: this.options.activemq_login,
+    passcode: this.options.activemq_password,
+  });
+
+  this.client.connect();
+  var self = this;
+  self.client.once('connected', function() {
+    console.log('Connected. Subscribe on', options.activemq_queue)
+    self.client.subscribe({
+      destination: options.activemq_queue,
+      ack: 'client'
+    }, function(body, headers){
+       console.log('Subscribed');
+    });
+  });
+
+  self.client.on('error', function(error_frame) {
+    console.error(error_frame.body);
+    self.disconnect();
+  });
+
+  self.client.on('message', function(message) {
+    self.client.ack(message.headers['message-id']);
+    console.log(message.headers['message-id'] ,message.body);
+    var body = yaml.safeLoad(message.body);
+    self.dispatch(body, function(error){
+      if(error){
+        return console.error(message.headers['message-id'], 'Error', error);
+      }
+      console.log(message.headers['message-id'], 'OK');
+    });
+  }
+}
+
+OORouter.prototype.disconnect = function(){
+  this.client.disconnect();
+}
+
+// message coming from activemq_routing_plugin
+OORouter.prototype.dispatch = function(message, cb){
+  var routes = this.routes;
+  var id = message.app_name + '-' + message.namespace;
+  var self = this;
+  switch(message.action){
+    case 'create_application':
+      routes[id] = {
+        app_name: message.app_name,
+        namespace: message.namespace
+      };
+
+      // HTTPS template is based on alias.
+      // Without an alias there is no nginx HTTPS configuration
+      // Generate an alias for $APP_DNS
+      var app_dns = message.app_name + '-' + message.namespace + '.' + options.domain;
+      routes[id].aliases[app_dns] = {
+        alias: app_dns
+      };
+      return cb();
+    case 'delete_application':
+      delete routes[id];
+      return async.parrallel([function(cb){
+        self.removeConfig(id, cb);
+      }, function(cb){
+        self.removeCertificates(id, cb);
+      }], cb)
+    case 'add_alias':
+      routes[id].aliases = routes[id].aliases || {} ;
+      routes[id].aliases[message.alias] = {};
+      routes[id].aliases[message.alias].alias = message.alias;
+      return this.updateConfig(id, routes[id], cb);
+    case 'remove_alias':
+      delete routes[id].aliases[message.alias];
+      return async.parrallel([function(cb){
+        self.updateConfig(id, routes[id], cb);
+      }, function(cb){
+        self.removeCertificate(id, message.alias, cb);
+      }], cb)
+    case 'add_ssl':
+      routes[id].aliases[message.alias].ssl = message.ssl;
+      routes[id].aliases[message.alias].private_key = message.private_key;
+      routes[id].aliases[message.alias].pass_phrase = message.pass_phrase;
+      return async.parrallel([function(cb){
+        self.updateConfig(id, routes[id], cb);
+      }, function(cb){
+        self.addCertificate(id, message.alias, message, cb);
+      }], cb)
+    case 'remove_ssl': 
+      delete routes[id].aliases[message.alias].ssl;
+      delete routes[id].aliases[message.alias].private_key;
+      delete routes[id].aliases[message.alias].pass_phrase;
+      return async.parrallel([function(cb){
+        self.updateConfig(id, routes[id], cb);
+      }, function(cb){
+        self.removeCertificate(id, message.alias, cb);
+      }], cb)
+    case 'add_gear': 
+      var socket = message.public_address + ':' + message.public_port;
+      if(/haproxy/.test(message.public_port_name)) return cb();
+      if(!/http/.test(message.types)) return cb();
+      routes[id].gears = routes[id].gears || [];
+      routes[id].gears[socket] = {
+        public_port_name: message.public_port_name,
+        public_address: message.public_address,
+        public_port: message.public_port,
+        protocols: message.protocols,
+        types: message.types,
+        mappings: message.mappings
+      };
+      return this.updateConfig(id, routes[id], cb);
+    case 'delete_gear':
+      var socket = message.public_address + ':' + message.public_port;
+      delete routes[id].gears[socket];
+      return this.updateConfig(id, routes[id], cb);
+    default:
+      return cb(new Error('action not known'));
+  }
+}
+
+// create or update nginx optionsuration file
+// echo $DATA > ${NGINX_CONF_DIR}/${APP_NAME}-${NAMESPACE}.conf
+OORouter.prototype.updateConfig = function(id, data, cb){
+  var nginxConfigFile = fs.join(this.options.nginxConfigDir, id + '.conf');
+  var nginxConfig = _.template(this.nginxConfigTmpl, _.extend({options: this.options}, data));
+
+  // Group gears by mapping for nginx templating
+  nginxConfig.mapping = {};
+  _(nginxConfig.gears).forEach(function(gear, socket){
+    gear.mappings.forEach(function(mapping){
+      nginxConfig.mapping[mapping.frontend] = nginxConfig.mapping[mapping.frontend] || {};
+      nginxConfig.mapping[mapping.frontend].frontend = mapping.frontend;
+      nginxConfig.mapping[mapping.frontend].backend = mapping.backend;
+      nginxConfig.mapping[mapping.frontend].gears = nginxConfig.mapping[mapping.frontend].gears || [];
+      nginxConfig.mapping[mapping.frontend].gears.push(gear);
+    });
+  });
+  nginxConfig.mapping = _(nginxConfig.mapping).values();
+  
+  fs.writeFile(nginxConfigFile, nginxConfig, cb);
+}
+
+// remove nginx optionsuration file
+// rm ${NGINX_CONF_DIR}/${APP_NAME}-${NAMESPACE}.conf
+OORouter.prototype.removeConfig = function(id, cb){
+  var nginxConfigFile = fs.join(this.options.nginxConfigDir, id + '.conf');
+  fs.unlink(nginxConfigFile, cb)
+}
+
+// write certificate files for an alias
+// mkdir ${NGINX_CONF_DIR}/${APP_NAME}-${NAMESPACE}
+// echo $KEY > ${NGINX_CONF_DIR}/${APP_NAME}-${NAMESPACE}/${ALIAS}.key
+// echo $CERT > ${NGINX_CONF_DIR}/${APP_NAME}-${NAMESPACE}/${ALIAS}.cert
+OORouter.prototype.addCertificate = function(id, alias, message, cb){
+  var certificatesDir = Path.join(this.options.nginxConfigDir, id);
+  async.serial([function(cb){
+    fs.exists(certificatesDir, function(error, exists){
+      if(error){
+        return cb(error);
+      }
+      if(exists){
+        return cb();
+      }
+      fs.mkdir(certificatesDir, cb);
+    });
+  }, function(cb){
+    var keyFile = Path.join(certificatesDir, alias + '.key');
+    fs.writeFile(keyFile, message.aliases[alias].private_key, cb);
+  }, function(cb){
+    var certFile = Path.join(certificatesDir, alias + '.cert');
+    fs.writeFile(certFile, message.aliases[alias].ssl, cb);
+  }], cb);
+}
+
+// delete certificate files for an alias
+// rm ${NGINX_CONF_DIR}/${APP_NAME}-${NAMESPACE}/${ALIAS}.key
+// rm ${NGINX_CONF_DIR}/${APP_NAME}-${NAMESPACE}/${ALIAS}.cert
+OORouter.prototype.removeCertificate = function(id, alias, cb){
+  var certificatesDir = Path.join(this.options.nginxConfigDir, id);
+  async.parrallel([function(cb){
+    var keyFile = Path.join(certificatesDir, alias + '.key');
+    fs.unlink(keyFile, cb);
+  }, function(cb){
+    var certFile = Path.join(certificatesDir, alias + '.cert');
+    fs.unlink(certFile, cb);
+  }], cb);
+}
+
+// delete every certificates files for an application
+// rm -fr ${NGINX_CONF_DIR}/${APP_NAME}-${NAMESPACE}
+OORouter.prototype.removeCertificates = function(id, cb){
+  var certificatesDir = Path.join(this.options.nginxConfigDir, id);
+  fs.rimraf(certificatesDir, cb);
+}
+
+// when regexp change, some nginx optionsurations and certificates has to be deleted.
+// clean the memory as well
+OORouter.prototype.onRegExpChange = function(cb){
+  var routes = Object.keys(this.routes);
+  var self = this;
+  async.forEach(routes, function(id, cb){
+    if(self.options.regExp.test(id)){
+      return cb();
+    }
+    delete self.routes[id];
+
+    async.parrallel([function(cb){
+      self.removeConfig(id, cb);
+    }, function(cb){
+      self.removeCertificates(id, cb);
+    }], cb);
+  }, function(error){
+    if(error){
+      return cb(error);
+    }
+    cb();
+  });
+}
